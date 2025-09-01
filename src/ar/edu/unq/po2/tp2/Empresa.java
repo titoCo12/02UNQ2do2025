14f3b65606cb1f97d30e27a722988f1c21a2ac4f
titoCo12 <joaquinfuentes170@gmail.com>
2025-08-31 22:50:11 -0300

    getters empresa

--- src/ar/edu/unq/po2/tp2/Empresa.java
+++ src/ar/edu/unq/po2/tp2/Empresa.java
@@ -1,5 +1,35 @@
 package ar.edu.unq.po2.tp2;
+import java.util.*;
 
 public class Empresa {
 
+	private String nombre;
+	private int CUIT;
+	private List<Empleado> empleados;
+	private List<Recibo> recibos;
+	
+	public Empresa(String nombre, int CUIT) {
+		this.nombre = nombre;
+		this.CUIT = CUIT;
+	}
+	
+	public void contratarEmpleado(Empleado emp) {
+		this.empleados.add(emp); 
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public int getCUIT() {
+		return this.CUIT;
+	}
+	
+	public List<Empleado> getEmpleados() {
+		return this.empleados;
+	}
+	
+	public List<Recibo> getRecibos() {
+		return this.recibos;
+	}
 }

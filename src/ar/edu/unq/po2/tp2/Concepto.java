02ede2765d76fbd261ca7f7cb8abf8991ed9cdd2
titoCo12 <joaquinfuentes170@gmail.com>
2025-09-01 16:32:03 -0300

    concepto y recibo

---
+++ src/ar/edu/unq/po2/tp2/Concepto.java
@@ -0,0 +1,24 @@
+package ar.edu.unq.po2.tp2;
+import ar.edu.unq.po2.tp2.*;
+
+
+public class Concepto {
+
+	private String nombre;
+	private Float valor;
+	
+	public Concepto(String nombre, Float valor) {
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public Float getValor() {
+		return this.valor;
+	}
+	
+	
+}
--- src/ar/edu/unq/po2/tp2/Recibo.java
+++ src/ar/edu/unq/po2/tp2/Recibo.java
@@ -1,5 +1,51 @@
 package ar.edu.unq.po2.tp2;
+import ar.edu.unq.po2.tp2.*;
+import java.util.*;
+import java.time.*;
+
 
 public class Recibo {
 
+	private String nombreEmpleado;
+	private String direccion;
+	private LocalDate fechaEmision;
+	private float sueldoBruto;
+	private float sueldoNeto;
+	private List<Concepto> conceptos;
+
+	
+	public Recibo(String nombreEmpleado, String direccion, LocalDate fechaEmision, float sueldoBruto, float sueldoNeto) {
+		this.nombreEmpleado = nombreEmpleado;
+		this.direccion = direccion;
+		this.fechaEmision = fechaEmision;
+		this.sueldoBruto = sueldoBruto;
+		this.sueldoNeto = sueldoNeto;
+	}
+	
+	
+	public void agregarConcepto(Concepto concepto) {
+		this.conceptos.add(concepto);
+	}
+	
+	
+	public String getNombre() {
+		return this.nombreEmpleado;
+	}
+	
+	public String getDireccion() {
+		return this.direccion;
+	}
+	
+	public LocalDate getFechaEmision() {
+		return this.fechaEmision;
+	}
+	
+	public float sueldoBruto() {
+		return this.sueldoBruto;
+	}
+	
+	public float sueldoNeto() {
+		return this.sueldoNeto;
+	}
+	
 }

bd215cc6d7ccb227376e7253254adb32c6b8a732
titoCo12 <joaquinfuentes170@gmail.com>
2025-09-08 00:00:35 -0300

    Multioperador terminado

--- src/ar/edu/unq/po2/tp3/Multioperador.java
+++ src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -1,5 +1,34 @@
 package ar.edu.unq.po2.tp3;
+import java.util.*;
+
+
 
 public class Multioperador {
 
+	private ArrayList<Integer> numeros;
+	
+	
+	public Integer sumaDeNumeros() {
+		Integer total = 0;
+		for (Integer n:numeros) {
+			total += n;
+		}
+		return total;
+	}
+	
+	
+	public Integer restaDeNumeros() {
+		return -this.sumaDeNumeros();
+	}
+	
+	
+	public Integer productoDeNumeros() {
+		Integer total = 1;
+		for (Integer n:numeros) {
+			total *= n;
+		}
+		return total;
+	}
+	
+	
 }

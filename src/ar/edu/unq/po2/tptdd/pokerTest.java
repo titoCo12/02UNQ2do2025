90a20becf6fcc26a7d0fef4fe747d7500c632a7b
titoCo12 <joaquinfuentes170@gmail.com>
2025-10-02 16:23:57 -0300

    base tdd

---
+++ src/ar/edu/unq/po2/tptdd/pokerTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tptdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class pokerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

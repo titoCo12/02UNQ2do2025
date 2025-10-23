591b12f115a5f60a2b0e85267e075c487e605806
titoCo12 <joaquinfuentes170@gmail.com>
2025-10-23 15:57:50 -0300

    Comentario Template

--- src/ar/edu/unq/po2/TemplateAdapter/Filtro.java
+++ src/ar/edu/unq/po2/TemplateAdapter/Filtro.java
@@ -2,6 +2,12 @@ package ar.edu.unq.po2.TemplateAdapter;
 
 import java.util.List;
 
+/*
+Esta todo Mal!!!!! No deberia ser interfaz deberia ser clase abstractca y todos los metodos
+ Deberian seguir el patron de iterar por cada pagina en wikipedia y ver si "esSimilar()" 
+ que seria el metodo hook. (estaba muy cansado cuando lo hice diablo)
+*/
+
 public interface Filtro {
 
 	public List<WikipediaPage> getSimilarPages(WikipediaPage page, List<WikipediaPage> wikipedia); 

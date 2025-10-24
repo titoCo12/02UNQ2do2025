73874fd1ba11251fd34b829bd939719990fda0a7
titoCo12 <joaquinfuentes170@gmail.com>
2025-10-24 00:27:01 -0300

    comentario template

--- src/ar/edu/unq/po2/TemplateAdapter/FiltroPEC.java
+++ src/ar/edu/unq/po2/TemplateAdapter/FiltroPEC.java
@@ -1,9 +1,7 @@
 package ar.edu.unq.po2.TemplateAdapter;
 
-import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
